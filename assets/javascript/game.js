872f80c6244959473bed4a9c575baa5289e5b62e
Wynn Chaney <wynn.chaney@wanco.com>
2019-05-10 15:39:51 -0600

    Added global variables to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+//Create global variables
+
+
+var targetScore = 0
+var yourScore = 0
+var wins = 0
+var losses = 0
+var randomNumber = 0
+
+randomNumber = generateRandom();
+//Create a function on page load that creates random number to hit,
+//generates random crystal values; assigns crytal values to crystals;
+//one of the function needs to generate random number; 
+$(document).ready(function() {
+    targetScore = (Math.floor(Math.random()* 100 + 19 ));
+    console.log("The random number is: " + randomNumber);
+    $("#random-number").text(targetScore);
+
+})
+
+
+
+
+// var crystalList = [{ src: "images/citrine.jpg", 
+// score: randomNumber},
+// {imgSrc: "images/lepidolite",
+// score: randomNumber},
+// {imgSrc: "images/rhodochrosite.jpg",
+// score: randomNumber},
+// {imgSrc:  "images/shattuckite.jpg",
+// score: randomNumber}]
+//object would have a score, an image a pointer to the crystal image, aka collection of key value pairs. it would be an array of objects
+
+
+//    for(vari = 0; i < crystalList.length; i++){
+//         var imgSrc = crystalList[i].imgSrc
+//         var score = crystalList[i].score
+
+//         $("#image-holder").append("<img src=" + imgSrc + " width='400px'>");
+   
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Crystal Collector
+//every-time page loads target score has a value
+//the crystals have an assigned value
+//what variables will I need
+//return is a keyword. crystal values need to change every game.
+//all four crystals have the same class
+//how to differieniate between the class
+//data-attribute data-crystalValue
+//create an array of objects
+
+
+
+// a click event; start/reset function a game
+//when writing code don't repeat yourself (dry) and make it reusable
+//DOM manipulation methods .html for targetScore
+
+//Global Variables:
+//wins, losses, target score, your score
+// var score = yourScore + crystalValue;
+//array of crystals loop through dynamically create, the photo use an object, each crystal has multiple pieces of information
+
+    
+
+
+
+
+//Random Number Function:
+
+    //assign a value for each crystal
+
+
+// Click event function
+
+
+//start/reset game (call this function in  an opportune space)
+    //clear out variables for or foreach
+    //set target score//assign a value for each crystal
+    // if yourScore is greater than target score,
+    // if yourScore 
